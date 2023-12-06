5cc3b81192f78e178d4cc9db471bf9126c99df35
bestemic <przemek.pawlik123@gmail.com>
2023-12-06 23:44:51 +0100

    Perl - database file option, new optional field

--- addresses_book/addresses_book.pl
+++ addresses_book/addresses_book.pl
@@ -27,48 +27,104 @@ if ($@) {
     exit 1;
 }
 
-Logic::create_database();
+my $file_name;
+my $working_mode="list";
+my $argument;
 
 if (@ARGV >= 1) {
     my $mode = $ARGV[0];
     if ($mode eq "-a" || $mode eq "--add") {
-        if (@ARGV >= 2) {
-            my $contact_info = decode_utf8($ARGV[1]);
-            Logic::add_contact($contact_info);
+        $working_mode="add";
+        if (@ARGV >= 2 && $ARGV[1] ne "-f" && $ARGV[1] ne "--file") {
+            $argument = decode_utf8($ARGV[1]);
+            if (@ARGV >= 3 && ($ARGV[1] eq "-f" || $ARGV[1] ne "--file")) {
+                if (@ARGV >= 4 && $ARGV[3] ne "") {
+                    $file_name = decode_utf8($ARGV[3]);
+                } else {
+                    print "Błąd: Brak nazwy pliku z danymi.\n";
+                    exit 1;
+                }
+            }
         } else {
             print "Błąd: Brak danych do dodania.\n";
             exit 1;
         }
     } elsif ($mode eq "-r" || $mode eq "--remove") {
-        if (@ARGV >= 2) {
-            my $contact_name = decode_utf8($ARGV[1]);
-            Logic::remove_contact($contact_name);
+        $working_mode="remove";
+        if (@ARGV >= 2 && $ARGV[1] ne "-f" && $ARGV[1] ne "--file") {
+            $argument = decode_utf8($ARGV[1]);
+            if (@ARGV >= 3 && ($ARGV[1] eq "-f" || $ARGV[1] ne "--file")) {
+                if (@ARGV >= 4 && $ARGV[3] ne "") {
+                    $file_name = decode_utf8($ARGV[3]);
+                } else {
+                    print "Błąd: Brak nazwy pliku z danymi.\n";
+                    exit 1;
+                }
+            }            
         } else {
             print "Błąd: Brak kontaktu do usunięcia.\n";
             exit 1;
         }
     } elsif ($mode eq "-s" || $mode eq "--search") {
-        if (@ARGV >= 2) {
-            my $contact_name = decode_utf8($ARGV[1]);
-            Logic::listOne($contact_name);
+        $working_mode="search";
+        if (@ARGV >= 2 && $ARGV[1] ne "-f" && $ARGV[1] ne "--file") {
+            $argument = decode_utf8($ARGV[1]);
+            if (@ARGV >= 3 && ($ARGV[1] eq "-f" || $ARGV[1] ne "--file")) {
+                if (@ARGV >= 4 && $ARGV[3] ne "") {
+                    $file_name = decode_utf8($ARGV[3]);
+                } else {
+                    print "Błąd: Brak nazwy pliku z danymi.\n";
+                    exit 1;
+                }
+            }
         } else {
             print "Błąd: Brak nazwy kontaktu do wyszukania.\n";
             exit 1;
         }
     } elsif ($mode eq "-l" || $mode eq "--list") {
-        if (@ARGV >= 2) {
-            my $max_contacts = decode_utf8($ARGV[1]);
-            Logic::listAll($max_contacts);
+        $working_mode="list";
+        if (@ARGV >= 2 && $ARGV[1] ne "-f" && $ARGV[1] ne "--file") {
+            $argument = decode_utf8($ARGV[1]);
+            if (@ARGV >= 3 && ($ARGV[1] eq "-f" || $ARGV[1] ne "--file")) {
+                if (@ARGV >= 4 && $ARGV[3] ne "") {
+                    $file_name = decode_utf8($ARGV[3]);
+                } else {
+                    print "Błąd: Brak nazwy pliku z danymi.\n";
+                    exit 1;
+                }
+            }
+        } elsif (@ARGV >= 2 && ($ARGV[1] eq "-f" || $ARGV[1] ne "--file")) {
+            if (@ARGV >= 3 && $ARGV[2] ne "") {
+                $file_name = decode_utf8($ARGV[2]);
+            } else {
+                print "Błąd: Brak nazwy pliku z danymi.\n";
+                exit 1;
+            }
+        }
+    } elsif ($mode eq "-f" || $mode eq "--file") {
+        if (@ARGV >= 2 && $ARGV[1] ne "") {
+            $file_name = decode_utf8($ARGV[1]);
         } else {
-            Logic::listAll();
+            print "Błąd: Brak nazwy pliku z danymi.\n";
+            exit 1;
         }
     } else {
         print "Błąd: Wprowadzono nieznany tryb pracy.\n";
         exit 1;
     }
-} else {
-    Logic::listAll();
 }
+
+Logic::create_database($file_name);
+
+if ($working_mode eq "add") {
+    Logic::add_contact($argument);
+} elsif ($working_mode eq "remove") {
+    Logic::remove_contact($argument);
+} elsif ($working_mode eq "search") {
+    Logic::listOne($argument);
+} else {
+    Logic::listAll($argument);
+} 
 
 sub show_help {
     print "Skrypt do zarządzania książką adresową.\n\n";
@@ -76,29 +132,35 @@ sub show_help {
     print "OPIS\n";
     print "\tSkryt działa jako książka adresowa. Do zarządzania bazą adresów używamy jednego z czterech trybów: 'add', 'remove', 'search', 'list'.\n"
     . "\tAby dodać nowy kontakt (lub kontakty) należy podać argument otoczony znakiem cudzysłowu \"dane\", gdzie dane to informacje oddzielone od\n" 
-    . "\tsiebie za pomocą \|. Najpierw podajemy nazwę, potem numer telefonu, a na końcu email. Nazwa jest wymagana, następne dwa elementy nie są \n"
-    . "\tobowiązkowe, ale potrzebny jest przynajmniej jeden. Drugi można pominąć (zachowując strukturę podawania danych, czyli np. Imię||Email).\n"
-    . "\tIstnieje możliwość dodania więcej niż jednego kontaktu na raz, w takim celu należy po wpisaniu pierwszego kompletu danych podać kolejny\n"
-    . "\toddzielając go znakiem |\. Dane te są następnie przechowywane w pliku data.txt który znajduje się w tym samym folderze co skrypt. W momencie\n"
-    . "\tgdy plik nie zostanie znaleziony będzie on utworzony. Aby usunąć kontakt z listy należy podać nazwę kontaktu do trybu 'remove'. Opcja 'search'\n"
-    . "\tpozwala na pobranie podanego kontaktu z bazy i wyświetlenie go. Opcja 'list' jest opcją domyślną i wyświetla wszystkie zapisane kontakty -\n"
-    . "\tdomyślnie wypisywana jest cała książka adresowa, aby wyświetlić mniej wystarczy podać jako argument liczbę oznaczającą limit.\n";
+    . "\tsiebie za pomocą \|. Najpierw podajemy nazwę, kolejno opis, potem numer telefonu, a na końcu email. Nazwa jest wymagana i musi być jednoczłonowa,\n"
+    . "\topis jest opcjonalny, a dwa ostatnie elementy nie są obowiązkowe, ale potrzebny jest przynajmniej jeden. Drugi można pominąć (zachowując\n"
+    . "\tstrukturę podawania danych, czyli np. Nick|Opis||Email). Istnieje możliwość dodania więcej niż jednego kontaktu na raz, w takim celu należy\n"
+    . "\tpo wpisaniu pierwszego kompletu danych podać kolejny oddzielając go znakiem |\. Dane te są następnie domyślnie przechowywane w pliku data.txt\n"
+    . "\tktóry znajduje się w tym samym folderze co skrypt. Możemy jednak ręcznie wskazywać na plik który przechowuje dane używająć flagi 'file' jako\n"
+    . "\tprzedostatni argument wywołania skryptu i podająć po niej nazwę pliku. W momencie gdy plik nie zostanie znaleziony będzie on utworzony. Aby\n"
+    . "\tusunąć kontakt z listy należy podać nazwę kontaktu do trybu 'remove'. Opcja 'search' pozwala na pobranie na podstawie podanej nazwy, kontaktu\n"
+    . "\tz bazy i wyświetlenie go. Opcja 'list' jest opcją domyślną i wyświetla wszystkie zapisane kontakty - domyślnie wypisywana jest cała książka\n"
+    . "\tadresowa, aby wyświetlić mniej wystarczy podać jako argument liczbę oznaczającą limit.\n";
     print "\n";
 
     print "UŻYCIE\n";
-    print"\t$0 [TRYBY] [ARGUMENT]\n\n";
+    print"\t$0 [TRYBY] [ARGUMENT] [FLAGI] [PLIK]\n\n";
 
     print "TRYBY\n";
-    print "\t-h, --help\tWyświtla pomoc\n";
     print "\t-a, --add\tDodaje kontakty, przyjmuje jeden wymagany argument - kontakty do dodania w formacie opisanym wyżej\n";
-    print "\t-s, --search\tWyświtla podany kontakt, przyjemuje jeden wymagany argument - nazwę kontaktu do wyświetlenia\n";
+    print "\t-s, --search\tWyświtla podany kontakt, przyjemuje jeden wymagany argument - jednoczłonową nazwę kontaktu do wyświetlenia\n";
     print "\t-l, --list\tWyświetla listę kontaktów, przyjmuje jeden opcjonalny argument - liczbę kontaktów do wyświetlanie, domyślnie pokazuje wszystkie kontakty\n";
-    print "\t-r, --remove\tUsuwa podany kontakt, przyjemuje jeden wymagany argument - nazwę kontaktu do usunięcia\n";
+    print "\t-r, --remove\tUsuwa podany kontakt, przyjemuje jeden wymagany argument - jednoczłonową nazwę kontaktu do usunięcia\n";
+    print "\n";
+
+    print "FLAGI\n";
+    print "\t-h, --help\tWyświtla pomoc\n";
+    print "\t-f, --file\tWskazuje nazwę pliku używaną do zadządzania książką adresową, przyjmuje jeden wymagany argument - nazwę pliku\n";
     print "\n";
 
     print "PRZYKŁADY\n";
-    print"\t$0 -a \"Anna Lewandowska|123456789||Robert Lewandowski||robert\@gmail.com|Jan|987654321|jan\@company.com\"\n";
-    print"\t$0 -l 2\n";
+    print"\t$0 -a \"alewa|Anna Lewandowska|123456789||robercik|Robert Lewandowski||robert\@gmail.com|JAN||987654321|jan\@company.com\"\n";
+    print"\t$0 -l 2 -f nowa_baza.txt\n";
     print"\t$0 -s Jan\n";
-    print"\t$0 -r Jan\n";
+    print"\t$0 -r Jan -f nowa_baza.txt\n";
 }
--- addresses_book/logic.pm
+++ addresses_book/logic.pm
@@ -10,6 +10,12 @@ my ($script_dir) = $0 =~ /^(.*)[\/\\]/;
 my $file_path = $script_dir . "/data.txt";
 
 sub create_database {
+    my $file_name = @_[0];
+
+    if ($file_name) {
+        $file_path = $script_dir . "/" . $file_name;
+    }
+
     if (! -e $file_path) {
         open my $file_handle, '>', $file_path or die "Błąd: Nie można stworzyć pliku $file_path.";
         close $file_handle;
@@ -22,11 +28,12 @@ sub load_contacts {
     open(my $data, '<', $file_path) or die "Błąd: Nie udało się otworzyć pliku $file_path."; 
     while (my $line = <$data>) { 
         chomp $line; 
-        my ($name, $phone, $email) = split(/\|/, $line);   
+        my ($name, $description, $phone, $email) = split(/\|/, $line);   
         push @contacts, {
-                name    => $name,
-                phone   => $phone,
-                email   => $email,
+                name            => $name,
+                description     => $description,
+                phone           => $phone,
+                email           => $email,
         };
     }
     close $data;
@@ -40,7 +47,7 @@ sub save_contacts {
 
     open(my $file, '>', $file_path) or die "Błąd: Nie udało się otworzyć pliku $file_path."; 
     for my $contact (@contacts) {
-        print $file "$contact->{name}|$contact->{phone}|$contact->{email}\n";
+        print $file "$contact->{name}|$contact->{description}|$contact->{phone}|$contact->{email}\n";
     }
     close $file;
 }
@@ -51,27 +58,33 @@ sub add_contact {
     my @new_contacts;
 
     my $name = '';
+    my $description = '';
     my $phone = '';
     my $email = '';
     
     for my $i (0 .. $#contact_infos) {
-        if ($i % 3 == 0) {
+        if ($i % 4 == 0) {
             $name = $contact_infos[$i];
         }
-        if ($i % 3 == 1) {
+        if ($i % 4 == 1) {
+            $description = $contact_infos[$i];
+        }
+        if ($i % 4 == 2) {
             $phone = $contact_infos[$i];
         }
-        if ($i % 3 == 2) {
+        if ($i % 4 == 3) {
             $email = $contact_infos[$i];
 
             if ($name ne '' || $phone ne '' || $email ne '') {
                 validate_contact_data($name, $phone, $email);
                 push @new_contacts, {
-                    name    => $name,
-                    phone   => $phone,
-                    email   => $email,
+                    name            => $name,
+                    description     => $description,
+                    phone           => $phone,
+                    email           => $email,
                 };
                 $name = '';
+                $description = '';
                 $phone = '';
                 $email = '';
             }
@@ -80,9 +93,10 @@ sub add_contact {
     if ($name ne '' || $phone ne '' || $email ne '') {
         validate_contact_data($name, $phone, $email);
         push @new_contacts, {
-            name    => $name,
-            phone   => $phone,
-            email   => $email,
+            name            => $name,
+            description     => $description,
+            phone           => $phone,
+            email           => $email,
         };
     }
 
@@ -136,16 +150,16 @@ sub listAll {
     }
 
     if (@saved_contacts) {
-        print "+--------------------------------+-----------+--------------------------------+\n";
-        printf "| %-30s | %-9s | %-30s |\n", 'Nazwa', 'Telefon', 'Email';
-        print "+--------------------------------+-----------+--------------------------------+\n";
+        print "+--------------------------------+----------------------------------------------------+-----------+--------------------------------+\n";
+        printf "| %-30s | %-50s | %-9s | %-30s |\n", 'Nazwa', 'Opis', 'Telefon', 'Email';
+        print "+--------------------------------+----------------------------------------------------+-----------+--------------------------------+\n";
 
         for my $contact (@saved_contacts) {
-            printf "| %-30s | %-9s | %-30s |\n", $contact->{name}, $contact->{phone}, $contact->{email};
+            printf "| %-30s | %-50s | %-9s | %-30s |\n", $contact->{name}, $contact->{description}, $contact->{phone}, $contact->{email};
         }
 
-        print "+--------------------------------+-----------+--------------------------------+\n";
-    } else {
+        print "+--------------------------------+----------------------------------------------------+-----------+--------------------------------+\n";
+        } else {
         print "Nie znaleziono kontaktów.\n";
     }
 }
@@ -159,6 +173,7 @@ sub listOne {
     for my $contact (@saved_contacts) {
         if ($contact->{name} eq $contact_name) {
             print "Nazwa: $contact->{name}\n";
+            print "Opis: $contact->{description}\n" if $contact->{description};
             print "Telefon: $contact->{phone}\n" if $contact->{phone};
             print "Email: $contact->{email}\n" if $contact->{email};
             $contact_found = 1;
@@ -175,6 +190,11 @@ sub validate_contact_data {
     if (@_[0] eq '') {
         print "Błąd: Nazwa kontaktu jest wymagana.\n";
         exit 1;
+    } else {
+        if (!(@_[0] =~ /^\S+$/)) {
+            print "Błąd: Nazwa kontaktu nie jest jednoczłonowa.\n";
+            exit 1;
+        }
     }
     if (@_[1] eq '' && @_[2] eq '') {
         print "Błąd: Wymagane jest podanie minimum jednej formy kontaktu (telefon, email).\n";
